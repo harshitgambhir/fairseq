bf9265edd92ebd7379ae3830e2bb19cdfd6fb594
harshitgambhir <harshitgambhir88@gmail.com>
2020-07-11 11:31:15 +0530

    check

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -245,6 +245,11 @@ class Trainer(object):
         if bexists:
             state = checkpoint_utils.load_checkpoint_to_cpu(filename)
 
+            encoder_pos = state['model']['encoder.embed_positions.weight']
+            to_append = encoder_pos[2:]
+            new_encoder_pos = torch.cat((encoder_pos, to_append))
+            state['model']['encoder.embed_positions.weight'] = new_encoder_pos
+
             # load model parameters
             try:
                 self.get_model().load_state_dict(
